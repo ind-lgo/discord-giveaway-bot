1f3547aa9cc48aaf4419a48097d87dad395d5b51
leon2241 <86055143+leon2241@users.noreply.github.com>
2022-01-12 14:08:10 +0300

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -7,7 +7,7 @@ module.exports = async (client, message) => {
     let args = message.content.slice(config["Bot_Info"].prefix.length).trim().split(/ +/g);
     let cmd = args.shift().toLowerCase();
 
-    if (message.content.match(new RegExp(`^<@!?${client.user.id}>( |)$`))) return message.channel.send(`Try ${config["Bot_Info"].prefix}help to see a list of my commands.`)
+    if (message.content.match(new RegExp(`^<@!?${client.user.id}>( |)$`))) return message.channel.send(`Try ${config["Bot_Info"].prefix} type help to see a list of my commands.`)
     if (!message.content.startsWith(config["Bot_Info"].prefix)) return;
     let commandfile = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd))
     if (commandfile) commandfile.run(client, message, args)
